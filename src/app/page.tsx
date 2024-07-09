836c6b7a60990aa67c9203620254ff5c3f5a1ff5
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-07-09 09:04:58 +0300

    feat: move links to the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href="/performance">Performance Page</Link>
-        <Link href="/reliability">Reliability Page</Link>
-        <Link href="/scale">Scale Page</Link>
-      </div>
       Home Page
     </div>  
   );
